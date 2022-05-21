cd19eebac694a09acb3d318157465e2bcb10f1f5
mahbub09 <mahabub895alam@gmail.com>
2022-05-22 01:19:51 +0600

    daisyui configed

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        sinewytheme: {
+          primary: "#0d7594",
+          secondary: "#0FCFEC",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
