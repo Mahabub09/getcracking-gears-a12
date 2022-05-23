2202b8b19d79d82a79c03d91ad3423b61f83278a
mahbub09 <mahabub895alam@gmail.com>
2022-05-24 05:42:40 +0600

    last before sleep

--- src/Pages/AddReview/AddReview.js
+++ src/Pages/AddReview/AddReview.js
@@ -6,7 +6,21 @@ import auth from '../../firebase.init';
 const AddReview = () => {
     const [user] = useAuthState(auth);
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/review`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
     return (
         <div> <h1 className='text-neutral underline font-serif underline-offset-8 text-3xl text-center my-3 '>Your Opinion Means A Lot To Us</h1>
             <div className='flex flex-col items-center justify-center transition ease-in-out delay-300 hover:-translate-y-1 hover:scale-90'>
@@ -16,6 +30,7 @@ const AddReview = () => {
             <form className='flex flex-col w-50 mx-auto   space-y-3' onSubmit={handleSubmit(onSubmit)}>
                 <input className='border border-dark p-2 rounded' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
                 <input className='border border-dark p-2 rounded' placeholder='LOCATION' {...register("location",)} />
+                <input placeholder='Image URL' className='border border-dark py-2 rounded' value={user?.photoURL} {...register("img")} readOnly />
 
                 <input className='border border-dark p-2 rounded' placeholder='RATING' type="number" {...register("rating", { min: 1, max: 5 })} />
                 <textarea className='border border-dark p-2 rounded' placeholder='REVIEW' {...register("review",)} />
